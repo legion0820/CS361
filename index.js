98b9c3109de2505105b3bac5ebc8093fbdb98c64
Leon Ong <ongl@flip1.engr.oregonstate.edu>
2023-11-06 21:02:48 -0800

    First commit of prototpye of CS361 assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+// const backdrop = document.getElementById("modal-backdrop");
+// const beanCounterModal = document.getElementById("bean-counter-modal");
+// const addLogButton = document.getElementById("add-log");
+// const xClose = document.getElementById("modal-close");
+
+// // Function to open the modal
+// function openModal() {
+//     modalBackdrop.classList.remove("hidden");
+//     beanCounterModal.classList.remove("hidden");
+// }
+
+// // Function to close the modal
+// function closeModal() {
+//     modalBackdrop.classList.add("hidden");
+//     beanCounterModal.classList.add("hidden");
+// }
+
+// // Event listener to open the modal when the "Add Log" button is clicked
+// addLogButton.addEventListener("click", openModal);
+
+// // Event listener to close the modal when the close button is clicked
+// modalCloseButton.addEventListener("click", closeModal);
+
+// backdrop.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// buttonCancel.onclick = function(event) {
+//     if (event.target == buttonCancel) {
+//         modal.style.display = "none";
+//         backdrop.style.display = "none";
+//         textInput.value = "";
+//         photoInput.value = "";
+//         priceInput.value = "";
+//         cityInput.value = "";
+        
+//     }
+// }
